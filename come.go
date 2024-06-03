93cb2914cbc874d151ddf75e60db5addfdf7277b
Bhargav4037 <113230592+Bhargav4037@users.noreply.github.com>
2024-06-03 05:11:57 +0000

    Add files via upload

---
+++ come.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mai() {
+	var a int
+	a = 5
+	fmt.Println("Hello world")
+	fmt.Println(a)
+}
---
+++ repo.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Man() {
+	fmt.Println("Hello world")
+}
